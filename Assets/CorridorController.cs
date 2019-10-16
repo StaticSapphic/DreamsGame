c1c2315b909065a4040a28a1ce40517682acf597
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-10-16 18:00:09 +0100

    END SESSION

---
+++ Assets/CorridorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorController : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 Offset;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.Translate(Offset);
+        Debug.Log("Teleported!");
+        Debug.Log(other.transform);
+    }
+}
