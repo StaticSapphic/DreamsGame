b6616e961fc323e41c32e5aa2f4eb117ff853124
MATTHEW SHAW <matthew.w.shaw74@gmail.com>
2019-10-21 00:13:29 +0100

    idfk

---
+++ Assets/Scripts/TrophyInteract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophyInteract : Interactable
+{
+    public GameObject floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        floor.SetActive(false);
+    }
+}
