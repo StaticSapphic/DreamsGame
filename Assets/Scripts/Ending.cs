371b9ee9d1c62e8389b0e2380ff8125dd553e7ad
asdsad <asdasdasd>
2019-10-21 09:50:13 +0100

    Added killplane to bedroom

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject credits;
+    public GameObject HUD;
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!won)
+        {
+            if (player.transform.position.y <= -500)
+            {
+                //We won, lock player, lock camera, play credits, profit?
+                won = true;
+                player.GetComponent<Rigidbody>().useGravity = false;
+                player.GetComponent<Rigidbody>().isKinematic = false;
+                player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                credits.SetActive(true);
+                HUD.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Killplane.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killplane : MonoBehaviour
+{
+    public Vector3 ResetPosition;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = ResetPosition;
+    }
+}
