1493d0ac17aa37d1317a43fe30924f497f5a55d0
MATTHEW SHAW <matthew.w.shaw74@gmail.com>
2019-10-24 00:44:24 +0100

    Bed warp added

--- Assets/Scripts/BedInteract.cs
+++ Assets/Scripts/BedInteract.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class BedInteract : MonoBehaviour
+public class BedInteract : Interactable
 {
+    public string NextScene = "";
+
+    public override void Interact()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadYourAsyncScene()
     {
-        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(NextScene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
