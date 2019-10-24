1cf8ace6a858d5d44ffc52e5f16c572692569733
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-10-24 13:38:44 +0100

    Added rainbow toilets that like to error

---
+++ Assets/Scripts/ShowAudioVolume.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ShowAudioVolume : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float updateStep = 0.1f;
+    public int sampleDataLength = 1024;
+
+    private float currentUpdateTime = 0f;
+
+    public float clipLoudness;
+    private float[] clipSampleData;
+
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        clipSampleData = new float[sampleDataLength];
+    }
+
+
+    void Update()
+    {
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep)
+        {
+            currentUpdateTime = 0f;
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples);
+
+            clipLoudness = 0f;
+            foreach (var sample in clipSampleData)
+            {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= sampleDataLength;
+        }
+    }
+}
---
+++ Assets/Scripts/Toilet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShowAudioVolume))]
+public class Toilet : MonoBehaviour
+{
+    public GameObject plane;
+    private ShowAudioVolume volumeGetter;
+    private Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeGetter = GetComponent<ShowAudioVolume>();
+        rend = plane.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.color = Color.HSVToRGB(volumeGetter.clipLoudness, 1, 1);
+    }
+}
