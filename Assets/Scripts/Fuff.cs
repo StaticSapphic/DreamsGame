997106849b4743b3d6fdc470571ef955f6df9adc
MATTHEW SHAW <matthew.w.shaw74@gmail.com>
2019-10-24 04:57:57 +0100

    Fuff quack

---
+++ Assets/Scripts/Fuff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Fuff : MonoBehaviour
+{
+    public float LookDuration;
+    private float currentLookTime;
+    private bool looking = false;
+
+    void OnBecameInvisible()
+    {
+        currentLookTime = 0;
+        looking = false;
+    }
+
+    void OnBecameVisible()
+    {
+        looking = true;
+    }
+
+    void Update()
+    {
+        if (looking)
+        {
+            currentLookTime += Time.deltaTime;
+        }
+        if (currentLookTime >= LookDuration)
+        {
+            currentLookTime = 0;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
