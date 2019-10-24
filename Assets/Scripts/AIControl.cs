24d261caa151cac7fb15d1e1745294adb29ca59e
atdeJimmyG <jamesgilljames9@gmail.com>
2019-10-24 19:07:00 +0100

    Added AI scripts, and teddybear animation

---
+++ Assets/Scripts/AIControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControl : MonoBehaviour
+{
+    Transform playerTransform;
+    UnityEngine.AI.NavMeshAgent myNavmesh;
+    public float checkRate = 0.01f;
+    float nextCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (GameObject.FindWithTag("Player").activeInHierarchy)
+        {
+            playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+        myNavmesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            FollowPlayer();
+        }
+
+    }
+
+    void FollowPlayer()
+    {
+        myNavmesh.transform.LookAt(playerTransform);
+        myNavmesh.destination = playerTransform.position;
+    }
+}
